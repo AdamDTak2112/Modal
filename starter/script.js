aa3308b91ff568b8fe7d9b1d5a7ba7a5ab1b6945
Adam Tak <adam.tak2112@gmail.com>
2021-02-01 10:46:11 -0700

    add event listeners to modal buttons

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+// Showing how you can assign querySelector results to a variable - in this case a const as it is going to be unchanging throughout.
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+console.log(btnsShowModal);
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+    btnsShowModal[i].addEventListener('click',
+        function () {
+            console.log(`Button #${i+1} clicked`);
+            modal.classList.remove('hidden');
+        });
+}
